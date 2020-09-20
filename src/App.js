c1f7dfc55b53f8b546097cfa5b8326c7ccb952be
AlbertoReal <alberto.real@dxcfds.com>
2020-09-20 19:02:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Suspense
+} from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import {
+  useTranslation
+} from "react-i18next";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// page uses the hook
+function Page() {
+  const {
+    t
+  } = useTranslation();
+
+  return ( <
+    div className = 'App' >
+    <
+    div className = 'row' >
+    <
+    div className = 'col' >
+    <
+    div className = 'alert alert-info' > {
+      t("title")
+    } < /div>{" "} < /
+    div > {
+      " "
+    } <
+    /div>{" "} < /
+    div >
   );
 }
 
-export default App;
+// loading component for suspense fallback
+const Loader = () => ( <
+  div className = 'App' >
+  <
+  img src = {
+    logo
+  }
+  className = 'App-logo'
+  alt = 'logo' / >
+  <
+  div > loading... < /div>{" "} < /
+  div >
+);
+
+// here app catches the suspense from page in case translations are not yet loaded
+export default function App() {
+  return ( <
+    Suspense fallback = {
+      <
+      Loader / >
+    } >
+    <
+    Page / >
+    <
+    /Suspense>
+  );
+}
